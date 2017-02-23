da209b376d40e0a6a1e237c74f4fb42fd866bc90
Alexander Bean Apmann <caboose200@gmail.com>
2017-02-23 16:48:03 -0500

    weqwe

--- src/muratproject1/simulation/Simulator.java
+++ src/muratproject1/simulation/Simulator.java
@@ -8,9 +8,16 @@ public class Simulator {
 	
 	public Report runSimulation(int days){
 		
+		//initialize
 		Report report = new Report();
 		
 		
+		
+		//run
+		for(int i = 0; i < days; i++){
+			
+			//do sim
+		}
 		
 		
 		return report;
