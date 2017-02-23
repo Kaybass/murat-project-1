c0cacdc27e00f507ed9ca8faf28ceebbdd8a58e0
Joe Listro <jlistro@gmail.com>
2017-02-23 17:37:42 -0500

    Store updates

--- src/muratproject1/people/Store.java
+++ src/muratproject1/people/Store.java
@@ -9,6 +9,31 @@ public class Store {
 	private ArrayList<Movie> Movies;
 	
 	public Store(ArrayList<Movie> movies){
+		setMovies(movies);
+	}
+	
+	public int numMovies() {
 		
+		return Movies.size();
+	}
+	
+	public Movie getMovie(String movieName) {
+		
+		for(int i = 0; i < Movies.size(); i++) {
+			if(Movies.get(i).getName() == movieName) {
+				return Movies.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	public Movie getMovie(int index) {
+		
+		return Movies.get(index);
+	}
+
+	private void setMovies(ArrayList<Movie> movies) {
+		Movies = movies;
 	}
 }
