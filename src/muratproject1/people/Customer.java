c8e27839886da8fcb2de9c22fcead6897df3e9d6
Alexander Bean Apmann <caboose200@gmail.com>
2017-02-23 15:45:40 -0500

    Speedy speed boiiiiiiiiii

--- src/muratproject1/people/Customer.java
+++ src/muratproject1/people/Customer.java
@@ -1,5 +1,14 @@
 package muratproject1.people;
 
+import muratproject1.people.behavior.Behavior;
+import muratproject1.transaction.Rental;
+
 public class Customer {
+	
+	private Rental activeRental;
+	
+	private String name;
+	
+	private Behavior customerBehavior;
 
 }
--- src/muratproject1/products/Movie.java
+++ src/muratproject1/products/Movie.java
@@ -1,5 +1,27 @@
 package muratproject1.products;
 
-public class Movie {
+import muratproject1.products.types.Type;
 
+public class Movie {
+	
+	private Type movieType;
+	
+	private String Name;
+	
+	public Movie(String name, Type type){
+		movieType = type;
+		Name = name;
+	}
+	
+	public double getMoneyDue(int daysRented){
+		return movieType.calculatePrice(daysRented);
+	}
+	
+	public String getName(){
+		return Name;
+	}
+	
+	public void changeType(Type type){
+		movieType = type;
+	}
 }
